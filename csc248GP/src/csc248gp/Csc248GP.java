3b4556ea24169f484faaf05babc1e59262f2a047
AHMAD NASRUDDIN BIN AHMAD NARASHID <2020619824@isiswa.uitm.edu.my>
2021-12-23 01:00:28 +0800

    input

--- csc248GP/src/csc248gp/Csc248GP.java
+++ csc248GP/src/csc248gp/Csc248GP.java
@@ -1,16 +1,29 @@
-
 package csc248gp;
-//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
-public class Csc248GP {
+//GIT PASSWORD: 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
 
-    
-    //hello world!
-    public static void main(String[] args) {
+import java.util.Scanner;
+public class Csc248GP 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
         
-        int j = 0;
+        Queue inputQ = new Queue();
         
-        
-        //asasdasd;
+        char loop = 'Y';
+        while (Character.toUpperCase(loop) == 'Y')
+        {
+            System.out.print("\nEnter Job: ");
+            String job = sc.nextLine();
+            System.out.print("Enter Arrival Time: ");
+            int aTime = Integer.parseInt(sc.nextLine());
+            System.out.print("Enter Burst Time: ");
+            int bTime = Integer.parseInt(sc.nextLine());
+            System.out.print("Do you to input more?: ");
+            loop = sc.nextLine().charAt(0);
+            
+            Job newJob = new Job(job, aTime, bTime);
+            inputQ.enqueue(newJob);
+        }
     }
-    
 }
