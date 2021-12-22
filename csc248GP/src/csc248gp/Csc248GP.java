d9fdec4c63cf5408b43bfa77f33e9945d41614da
AHMAD NASRUDDIN BIN AHMAD NARASHID <2020619824@isiswa.uitm.edu.my>
2021-12-23 01:54:43 +0800

    first Attempt (tak jadi tapi boleh lah)

--- csc248GP/src/csc248gp/Csc248GP.java
+++ csc248GP/src/csc248gp/Csc248GP.java
@@ -1,5 +1,4 @@
 package csc248gp;
-//GIT PASSWORD: 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
 
 import java.util.Scanner;
 public class Csc248GP 
@@ -9,11 +8,14 @@ public class Csc248GP
         Scanner sc = new Scanner(System.in);
         
         Queue inputQ = new Queue();
+        Queue tempInputQ = new Queue();
         Queue executeQ = new Queue();
+        Queue tempExecuteQ = new Queue();
         Queue waitQ = new Queue();
+        Queue doneQ = new Queue();
         
         //input
-        char loop = 'Y';
+        /*char loop = 'Y';
         while (Character.toUpperCase(loop) == 'Y')
         {
             System.out.print("\nEnter Job: ");
@@ -27,11 +29,43 @@ public class Csc248GP
             
             Job newJob = new Job(job, aTime, bTime);
             inputQ.enqueue(newJob);
-        }
+        }Penat nak input satu2 ;(*/
+        
+        //nak senang input siap2
+        Job A = new Job();
+        Job B = new Job();
+        Job C = new Job();
+        Job D = new Job();
+        Job E = new Job();
+        
+        A.setJob("A");
+        B.setJob("B");
+        C.setJob("C");
+        D.setJob("D");
+        E.setJob("E");
+        
+        A.setArrivalTime(1);
+        B.setArrivalTime(3);
+        C.setArrivalTime(6);
+        D.setArrivalTime(6);
+        E.setArrivalTime(9);
+        
+        A.setBurstTime(10);
+        B.setBurstTime(1);
+        C.setBurstTime(3);
+        D.setBurstTime(1);
+        E.setBurstTime(2);
+        
+        inputQ.enqueue(A);
+        inputQ.enqueue(B);
+        inputQ.enqueue(C);
+        inputQ.enqueue(D);
+        inputQ.enqueue(E);
         
         //time loop
+        boolean executeStatus = false;
         int time = 1;
-        for (int n=1; n<=time; n++)
+        for (int n=1; n<=18; n++)
         {
             System.out.println("\nTime: " +n);
             
@@ -40,10 +74,68 @@ public class Csc248GP
             {
                 temp = (Job)inputQ.dequeue();
                 
-                //if (temp)
+                if (temp.getArrivalTime()==n)
+                {
+                    executeQ.enqueue(temp);
+                    executeStatus = true;
+                }
+                else 
+                    tempInputQ.enqueue(temp);
+            }
+            if (executeQ.isEmpty())
+                System.out.println("End");
+            else
+                time++;
+            
+            while (!executeQ.isEmpty())
+            {
+                temp = (Job)executeQ.dequeue();
+                
+                if (n<=temp.getBurstTime() && executeStatus)
+                {
+                    System.out.println("Job "+temp.getJob()+" is executing...");
+                    if (n==(temp.getBurstTime()+temp.getHoldTime()+temp.getArrivalTime()))
+                    {
+                        executeStatus = false;
+                        doneQ.enqueue(temp);
+                    }
+                    else
+                        tempExecuteQ.enqueue(temp);
+                }
+                else 
+                {
+                    if (n==temp.getArrivalTime())
+                        System.out.println("Job "+temp.getJob()+" has arrived...");
+                    else
+                        System.out.println("Job "+temp.getJob()+" is in hold for "+temp.getHoldTime()+"ms...");
+                    
+                    waitQ.enqueue(temp);
+                }
             }
             
-            time++;
+            while (!waitQ.isEmpty())
+            {
+                temp = (Job)waitQ.dequeue(); 
+                
+                if (!tempExecuteQ.isEmpty())
+                {
+                    temp.setHoldTime(temp.getHoldTime()+1);
+                }
+                
+                tempExecuteQ.enqueue(temp);
+            }
+            
+            while (!tempInputQ.isEmpty())
+            {
+                temp = (Job)tempInputQ.dequeue();
+                inputQ.enqueue(temp);
+            }
+            
+            while (!tempExecuteQ.isEmpty())
+            {
+                temp = (Job)tempExecuteQ.dequeue();
+                executeQ.enqueue(temp);
+            }
         }
     }
 }
--- csc248GP/src/csc248gp/Job.java
+++ csc248GP/src/csc248gp/Job.java
@@ -6,6 +6,15 @@ public class Job {
     private String job;
     private int arrivalTime;
     private int burstTime;
+    private int holdTime;
+    
+    public Job()
+    {
+        job=null;
+        arrivalTime=0;
+        burstTime=0;
+        holdTime=0;
+    }
     
     public Job(String j,int a,int b){
         job=j;
@@ -13,7 +22,13 @@ public class Job {
         burstTime=b;
     }
     
+    public void setJob (String j){job = j;}
+    public void setArrivalTime(int a){arrivalTime = a;}
+    public void setBurstTime(int b){burstTime = b;}
+    public void setHoldTime(int h){holdTime = h;}
+    
     public String getJob(){return job;}
     public int getArrivalTime(){return arrivalTime;}
     public int getBurstTime(){return burstTime;}
+    public int getHoldTime(){return holdTime;}
 }
